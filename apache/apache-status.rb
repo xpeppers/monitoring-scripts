a98f414e163b66d00a10cb9c04fdc71c3540dbf9
xpmatteo <vaccari@pobox.com>
2010-12-26 16:42:50 +0100

    minor fixes add host name to file name specify a destination dir

---
+++ apache/apache-status.rb
@@ -0,0 +1,55 @@
+#!/usr/bin/env ruby
+
+require 'net/http'
+
+
+def get_status
+  req = Net::HTTP::Get.new('/server-status?auto')
+  res = Net::HTTP.start(@apache_host, @apache_port) {|http|
+	    http.request(req)
+  }
+  raise res.message unless Net::HTTPSuccess === res
+  res.body
+end
+
+def timestamp
+  Time.now.strftime("%Y/%m/%d %H:%M:%S")  
+end
+
+def current_date
+  Time.now.strftime("%Y-%m-%d")  
+end
+
+def parse_status status  
+  res = status.split
+  total_workers = res[1].to_i + res[3].to_i
+  "#{timestamp} #{res[0]} #{res[1]}/#{total_workers} "
+end
+
+def save_to_rolling_file tag, text
+  File.open("#{@log_dir}/#{tag}-#{current_date}.txt", "a") do |f|
+    f.puts text
+  end
+end
+
+def usage
+  puts "#{$0} <apache ip> <apache port> <log_dir>"
+  exit
+end 
+
+def parse_command_line
+  if ARGV.size < 3
+	  usage
+  end
+  @apache_host = ARGV[0]
+  @apache_port = ARGV[1]
+  @log_dir = ARGV[2]
+end
+
+
+parse_command_line
+while true
+ status_line = parse_status(get_status)
+ save_to_rolling_file "#{@apache_host}-apache-status", status_line
+ sleep 10
+end
--- tomcat/tomcat-status.rb
+++ tomcat/tomcat-status.rb
@@ -4,18 +4,19 @@ require 'net/http'
 require 'rexml/document'
 
 def usage
-  puts __FILE__ + " <server> <port> <username> <password>"
+  puts __FILE__ + " <server> <port> <username> <password> <logdir>"
   exit 
 end
 
 def parse_command_line
-  if ARGV.size < 4
+  if ARGV.size < 5
     usage
   end
   @tomcat_host=ARGV[0]
   @tomcat_port=ARGV[1]
   @tomcat_user=ARGV[2]
   @tomcat_password=ARGV[3]
+  @log_dir = ARGV[4]
 end
 
 def get_status
@@ -99,7 +100,7 @@ def find_connectors
   find_by_xpath("/status/connector").map do |element|
     name = element.attributes["name"]
     this_connector = "/status/connector[@name='#{name}']"
-    current_threads_busy = find_by_xpath("#{this_connector}/workers/worker[@stage!='K']").size
+    current_threads_busy = find_by_xpath("#{this_connector}/workers/worker[@stage!='K' and @stage!='R']").size
     max_threads = find_attribute_by_xpath("#{this_connector}/threadInfo", "maxThreads")
     processing_time = find_attribute_by_xpath("#{this_connector}/requestInfo", "processingTime")
     request_count = find_attribute_by_xpath("#{this_connector}/requestInfo", "requestCount")
@@ -136,16 +137,29 @@ def save_xml
 end
 
 def save_to_rolling_file tag, text
-  File.open("#{tag}-#{current_date}.txt", "a") do |f|
+  File.open("#{@log_dir}/#{tag}-#{current_date}.txt", "a") do |f|
     f.puts text
   end
 end
 
+def protect
+  while true
+    begin
+      yield
+    rescue Exception => e
+      puts e.to_s
+      sleep 5
+    end
+  end
+end
+
 parse_command_line
-while true
-  parse_xml(get_status)
-  save_to_rolling_file "tomcat-status", status_line    
-  save_to_rolling_file "tomcat-open-uris", add_time_stamp(working_url_list)
-  sleep 30
+protect do
+  while true
+    parse_xml(get_status)
+    save_to_rolling_file "#{@tomcat_host}-tomcat-status", status_line    
+    save_to_rolling_file "#{@tomcat_host}-tomcat-open-uris", add_time_stamp(working_url_list)
+    sleep 10
+  end
 end
 
